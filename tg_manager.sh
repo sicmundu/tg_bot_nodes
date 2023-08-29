0d544ed0f01b90eda9600b35da472d37966479b2
sicmundu <grodstrike@gmail.com>
2023-08-29 21:29:42 +0400

    Update tg_manager.sh

--- tg_manager.sh
+++ tg_manager.sh
@@ -26,7 +26,39 @@ function animate() {
     echo -e "\n"
 }
 
-# Проверка и установка Node.js и NPM
+
+
+#!/bin/bash
+
+# Цветовая палитра
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# Функция для вывода сообщения с рамкой
+function box_msg() {
+    local msg="$1"
+    local len=${#msg}
+    printf "\n%0.s*" $(seq 1 $((len+8)))
+    echo -e "\n*  ${YELLOW}$msg${NC}  *"
+    printf "%0.s*" $(seq 1 $((len+8)))
+    echo -e "\n"
+}
+
+# Функция анимации
+function animate() {
+    echo -n "$1 "
+    for i in $(seq 1 3); do
+        echo -n "."
+        sleep 0.5
+    done
+    echo -e "\n"
+}
+
+# Функция установки
+function install() {
+    # Проверка и установка Node.js и NPM
 if ! command -v node > /dev/null; then
     box_msg "Установка Node.js и NPM"
     animate "Установка"
@@ -49,8 +81,8 @@ fi
 # Создание каталога проекта
 box_msg "Создание каталога проекта"
 animate "Создание"
-sudo mkdir -p $HOME/.tg_bot_manager
-cd $HOME/.tg_bot_manager
+mkdir -p ~/.tg_bot_manager
+cd ~/.tg_bot_manager
 echo -e "${GREEN}Каталог создан.${NC}\n"
 
 # Скачивание файла server.js с Github
@@ -70,7 +102,7 @@ echo -e "${GREEN}Токен сохранен.${NC}\n"
 box_msg "Установка зависимостей"
 animate "Установка"
 npm init -y
-npm install express dotenv node-cache
+npm install express dotenv
 echo -e "${GREEN}Зависимости успешно установлены.${NC}\n"
 
 # Создание и запуск службы
@@ -95,3 +127,50 @@ sudo systemctl daemon-reload
 sudo systemctl enable tg_bot_manager
 sudo systemctl start tg_bot_manager
 echo -e "${GREEN}Сервер успешно установлен и запущен.${NC}\n"
+}
+
+# Функция удаления
+function uninstall() {
+    # Остановка и удаление службы
+    box_msg "Остановка и удаление службы"
+    animate "Остановка"
+    sudo systemctl stop tg_bot_manager
+    sudo systemctl disable tg_bot_manager
+    sudo rm /etc/systemd/system/tg_bot_manager.service
+    sudo systemctl daemon-reload
+    sudo systemctl reset-failed
+    echo -e "${GREEN}Служба остановлена и удалена.${NC}\n"
+
+    sleep 1
+
+    # Удаление каталога проекта
+    box_msg "Удаление каталога проекта"
+    animate "Удаление"
+    sudo rm -rf $HOME/.tg_bot_manager
+    echo -e "${GREEN}Каталог проекта удален.${NC}\n"
+
+    sleep 1
+}
+
+# Функция обновления токена
+function update_token() {
+    box_msg "Обновление токена"
+    echo "Пожалуйста, введите ваш новый токен:"
+    read -r new_token
+    echo "TOKEN=$new_token" > $HOME/.tg_bot_manager/.env
+    sudo systemctl restart tg_bot_manager
+    echo -e "${GREEN}Токен успешно обновлен.${NC}\n"
+}
+
+
+
+if [ "$1" == "install" ]; then
+    install
+elif [ "$1" == "uninstall" ]; then
+    uninstall
+elif [ "$1" == "token" ]; then
+    update_token $2
+else
+    echo -e "${RED}Ошибка: Неверный аргумент. Используйте 'install', 'uninstall' или 'token'.${NC}"
+    exit 1
+fi
